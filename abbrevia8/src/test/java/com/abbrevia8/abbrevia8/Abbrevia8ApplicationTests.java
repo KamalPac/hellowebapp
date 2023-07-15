0ea882cd4954b8109f956a88d8c9d5104b5577f2
KamalPac <43869407+KamalPac@users.noreply.github.com>
2023-07-15 00:41:19 -0400

    Add files via upload

---
+++ abbrevia8/src/test/java/com/abbrevia8/abbrevia8/Abbrevia8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abbrevia8.abbrevia8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Abbrevia8ApplicationTests {
+
+	
+	void contextLoads() {
+	}
+
+}
