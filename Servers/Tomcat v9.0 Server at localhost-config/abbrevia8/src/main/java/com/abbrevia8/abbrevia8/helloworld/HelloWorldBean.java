91f6c55ec69c1c69febb1bc34467d052239fe5bf
KamalPac <43869407+KamalPac@users.noreply.github.com>
2023-07-15 00:35:50 -0400

    Add files via upload

---
+++ Servers/Tomcat v9.0 Server at localhost-config/abbrevia8/src/main/java/com/abbrevia8/abbrevia8/helloworld/HelloWorldBean.java
@@ -0,0 +1,23 @@
+package com.abbrevia8.abbrevia8.helloworld;
+
+public class HelloWorldBean {
+
+	private String message;
+
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}
+}
