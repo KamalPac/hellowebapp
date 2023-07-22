566db9bdbd11f5dd8a2f0e023e3ef21b00db1d88
kamal <cpkbharathy@gmail.com>
2023-07-22 11:54:34 -0400

    test

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -3,4 +3,4 @@ cd  /home/ec2-user/server/abbrevia8/target/
 sudo java -jar  -Dserver.port=8000  *.jar > /dev/null 2> /dev/null   </dev/null &
 cd /home/ec2-user/server/abbrevia8/
 sudo docker build -t hellowebapp:latest .  > /dev/null 2> /dev/null   </dev/null
-sudo docker run -p 8002:8080 -it hellowebapp
+sudo docker run -p 8002:8080 -t hellowebapp
