aec2dc8afbace2822a889b7c239733a2cba33f81
kamal <cpkbharathy@gmail.com>
2023-07-25 00:18:09 -0400

    fixing docker run

--- scripts/server_stop.sh
+++ scripts/server_stop.sh
@@ -1,3 +1,3 @@
 # /usr/bin/env bash
 sudo pkill -f 'java -jar' > /dev/null 2> /dev/null   </dev/null
-sudo docker stop `docker ps -q`
+sudo docker stop `docker ps -q` > /dev/null 2> /dev/null   </dev/null
